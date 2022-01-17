7b4b1a3b5940218bb2a71ac768a8aa8719d67f74
Alan Mejia <mejiamartinezalan@gmail.com>
2022-01-17 14:55:40 -0600

    config server

---
+++ user-service/src/test/java/com/userservice/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userservice.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
